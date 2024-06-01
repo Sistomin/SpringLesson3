d2dc93d90ca9c1afa32475304929b82aaa42a8f2
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2024-06-01 15:36:08 +0700

    Initial commit

---
+++ src/main/java/ru/istomin/clientserver/FirstSpringServerProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.istomin.clientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringServerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/istomin/clientserver/FirstSpringServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.istomin.clientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
